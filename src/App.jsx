9b58d17428e722062dcbf252668b48de8eb2a420
scribblenovice <pasomba41@gmail.com>
2024-10-22 20:34:10 +0100

    birthday carousel for phanie

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       </h1>
       <WaterwheelCarousel />
       <div className="flex gap-3 items-center">
-        <img className="w-16 h-16 rounded-full" src={src} alt="" />
+        <img className="w-[70px] h-[70px] rounded-full" src={src} alt="" />
         <div className="flex flex-col gap-2">
-          <h1 className="text-white text-2xl font-bold glow">
+          <h1 className="text-white text-center text-2xl font-bold glow">
             Happy Birthday Phanie!
           </h1>
           <p className="text-white text-center glow font-bold tracking-widest">Cheers to another year of being amazing🥂</p>
