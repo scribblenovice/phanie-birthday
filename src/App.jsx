e8ca2955cd6d44067d7709fff3ac2bf1611bf349
scribblenovice <pasomba41@gmail.com>
2024-10-22 20:23:31 +0100

    birthday carousel for phanie

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import WaterwheelCarousel from "./pages/Carousel";
 import src from "/birthday-bday.gif";
 function App() {
   return (
-    <div className="md:hidden h-screen flex flex-col gap-2">
+    <div className="md:hidden max-w-screen max-h-screen flex flex-col">
       <h1 className="text-center text-white font-bold flex gap-4 justify-center mt-5">
         <i class="ri-heart-fill"></i>22/10/2024<i class="ri-heart-fill"></i>
       </h1>
       <WaterwheelCarousel />
-      <div className="flex gap-5 items-center">
-        <img className="w-20 h-20 rounded-full" src={src} alt="" />
+      <div className="flex gap-3 items-center">
+        <img className="w-16 h-16 rounded-full" src={src} alt="" />
         <div className="flex flex-col gap-2">
           <h1 className="text-white text-2xl font-bold glow">
             Happy Birthday Phanie!
